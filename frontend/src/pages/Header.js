3dbf85ee5910c6018c8c8babf1b37f9c75925a63
Naveen Gupta <141839367+naveeng0602@users.noreply.github.com>
2024-04-24 18:05:01 +0530

    update

--- frontend/src/pages/Header.js
+++ frontend/src/pages/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
                 <path d="M6 2v.341C3.67 3.165 2 5.388 2 8v5.5A2.5 2.5 0 0 0 4.5 16h7a2.5 2.5 0 0 0 2.5-2.5V8a6.002 6.002 0 0 0-4-5.659V2a2 2 0 1 0-4 0m2-1a1 1 0 0 1 1 1v.083a6.04 6.04 0 0 0-2 0V2a1 1 0 0 1 1-1m0 3a4 4 0 0 1 3.96 3.43.5.5 0 1 1-.99.14 3 3 0 0 0-5.94 0 .5.5 0 1 1-.99-.14A4 4 0 0 1 8 4M4.5 9h7a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5"/>
               </svg> */}
               <div className='img' style={{borderRadius:10}}>
-              <img src={Logo} width={80} style={{borderRadius:50}} ></img>
+              <img src={Logo} alt ={{}} width={80} style={{borderRadius:50}} ></img>
               </div>
               
               <span className='mx-2 text-light lh-1 fw-semibold'>
