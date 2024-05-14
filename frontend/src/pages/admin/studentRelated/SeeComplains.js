49f2bd4c5b448e58620a473586f2058c50ee4292
Naveen Gupta <141839367+naveeng0602@users.noreply.github.com>
2024-05-14 21:07:18 +0530

    update

--- frontend/src/pages/admin/studentRelated/SeeComplains.js
+++ frontend/src/pages/admin/studentRelated/SeeComplains.js
@@ -1,19 +1,18 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  Paper, Box, Checkbox,IconButton
-} from '@mui/material';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Paper, Box, Checkbox, IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { getAllComplains } from '../../../redux/complainRelated/complainHandle';
-import TableTemplate from '../../../components/TableTemplate';
-import { deleteUser } from '../../../redux/userRelated/userHandle';
+import { getAllComplains } from "../../../redux/complainRelated/complainHandle";
+import TableTemplate from "../../../components/TableTemplate";
+import { deleteUser } from "../../../redux/userRelated/userHandle";
 
 const SeeComplains = () => {
-
-  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };  
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
   const dispatch = useDispatch();
-  const { complainsList, loading, error, response } = useSelector((state) => state.complain);
-  const { currentUser } = useSelector(state => state.user)
+  const { complainsList, loading, error, response } = useSelector(
+    (state) => state.complain
+  );
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     dispatch(getAllComplains(currentUser._id, "Complain"));
@@ -23,60 +22,74 @@ const SeeComplains = () => {
     console.log(error);
   }
   const deleteHandler = (deleteID, address) => {
-    dispatch(deleteUser(deleteID, address))
-        .then(() => {
-            dispatch(getAllComplains(currentUser._id, "Complain"));
-        })
-}
+    dispatch(deleteUser(deleteID, address)).then(() => {
+      dispatch(getAllComplains(currentUser._id, "Complain"));
+    });
+  };
 
   const complainColumns = [
-    { id: 'user', label: 'User', minWidth: 170 },
-    { id: 'complaint', label: 'Complaint', minWidth: 100 },
-    { id: 'date', label: 'Date', minWidth: 170 },
+    { id: "user", label: "User", minWidth: 170 },
+    { id: "complaint", label: "Complaint", minWidth: 100 },
+    { id: "date", label: "Date", minWidth: 170 },
   ];
-  const complainRows = complainsList && complainsList.length > 0 && complainsList.map((complain) => {
-    const date = new Date(complain.date);
-    const dateString = date.toString() !== "Invalid Date" ? date.toISOString().substring(0, 10) : "Invalid Date";
-    return {
-      user: complain.user.name,
-      complaint: complain.complaint,
-      date: dateString,
-      id: complain._id,
-    };
-  });
+  const complainRows =
+    complainsList &&
+    complainsList.length > 0 &&
+    complainsList.map((complain) => {
+      const date = new Date(complain.date);
+      const dateString =
+        date.toString() !== "Invalid Date"
+          ? date.toISOString().substring(0, 10)
+          : "Invalid Date";
+      return {
+        user: complain.user.name,
+        complaint: complain.complaint,
+        date: dateString,
+        id: complain._id,
+      };
+    });
 
   const ComplainButtonHaver = ({ row }) => {
     return (
       <>
         <>
-                <IconButton onClick={() => deleteHandler(row.id, "Complain")}>
-                    <DeleteIcon color="error" />
-                </IconButton>
-            </>
+          <IconButton onClick={() => deleteHandler(row.id, "Complain")}>
+            <DeleteIcon color="error" />
+          </IconButton>
+        </>
       </>
     );
   };
 
   return (
     <>
-      {loading ?
+      {loading ? (
         <div>Loading...</div>
-        :
+      ) : (
         <>
-          {response ?
-            <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '26px' }}>
+          {response ? (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "26px",
+              }}
+            >
               No Complains Right Now
             </Box>
-            :
-            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-              {Array.isArray(complainsList) && complainsList.length > 0 &&
-                <TableTemplate buttonHaver={ComplainButtonHaver} columns={complainColumns} rows={complainRows} />
-              }
+          ) : (
+            <Paper sx={{ width: "100%", overflow: "hidden" }}>
+              {Array.isArray(complainsList) && complainsList.length > 0 && (
+                <TableTemplate
+                  buttonHaver={ComplainButtonHaver}
+                  columns={complainColumns}
+                  rows={complainRows}
+                />
+              )}
             </Paper>
-          }
+          )}
         </>
-      }
-      
+      )}
     </>
   );
 };
